afb827e96bf4b4e286a6d7af8b6be67d4a64ee6a
Vadim Ryabov <mr.holeyko@gmail.com>
2022-10-07 19:00:45 +0300

    development main functionality of web/hw3

--- Web/hw3/src/main/java/ru/itmo/wp/servlet/CaptchaFilter.java
+++ Web/hw3/src/main/java/ru/itmo/wp/servlet/CaptchaFilter.java
@@ -18,51 +18,73 @@ public class CaptchaFilter extends HttpFilter {
     private final String CAPTCHA_PASSED_ATTRIBUTE_TEXT = "Captcha-Passed";
     private final String CAPTCHA_ANSWER_ATTRIBUTE_TEXT = "Captcha-Right-Answer";
     private final String LAST_URI_ATTRIBUTE_TEXT = "Last-URI";
+    private final String CAPTCHA_ATTRIBUTE_TEXT = "Captcha";
 
     @Override
     protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpSession session = request.getSession();
-        if (request.getMethod().equals("GET"))  {
-            if (session.getAttribute(this.CAPTCHA_PASSED_ATTRIBUTE_TEXT) == null) {
-                session.setAttribute(this.LAST_URI_ATTRIBUTE_TEXT, request.getRequestURI());
-                redirectToCaptcha(request, response);
-            } else {
-                chain.doFilter(request, response);
-            }
+        if (session.getAttribute(this.CAPTCHA_PASSED_ATTRIBUTE_TEXT) != null) {
+            chain.doFilter(request, response);
+        } else if (request.getMethod().equals("GET")) {
+            session.setAttribute(this.LAST_URI_ATTRIBUTE_TEXT, request.getRequestURI());
+            redirectToCaptcha(request, response, false);
         } else if (request.getMethod().equals("POST")) {
-            if (
-                    session.getAttribute(this.CAPTCHA_ANSWER_ATTRIBUTE_TEXT) != null &&
-                            request.getParameter(this.CAPTCHA_USER_ANSWER_PARAMETER_TEXT) != null &&
-                            request.getParameter(this.CAPTCHA_USER_ANSWER_PARAMETER_TEXT).equals(session.getAttribute(this.CAPTCHA_ANSWER_ATTRIBUTE_TEXT))
-            ) {
-                session.setAttribute(this.CAPTCHA_PASSED_ATTRIBUTE_TEXT, "true");
+            String uri = request.getRequestURI();
+            if (uri.equals("/captcha/check")) {
+                if (
+                        session.getAttribute(this.CAPTCHA_ANSWER_ATTRIBUTE_TEXT) != null &&
+                                request.getParameter(this.CAPTCHA_USER_ANSWER_PARAMETER_TEXT) != null &&
+                                request.getParameter(this.CAPTCHA_USER_ANSWER_PARAMETER_TEXT).equals(session.getAttribute(this.CAPTCHA_ANSWER_ATTRIBUTE_TEXT))
+                ) {
+                    session.setAttribute(this.CAPTCHA_PASSED_ATTRIBUTE_TEXT, "true");
 
-                String uri = (String) session.getAttribute(this.LAST_URI_ATTRIBUTE_TEXT);
-
+                    String lastUri = (String) session.getAttribute(this.LAST_URI_ATTRIBUTE_TEXT);
+                    response.setStatus(303);
+                    response.setHeader("Location", lastUri);
+                } else {
+                    redirectToCaptcha(request, response, true);
+                }
+            } else if (uri.equals("/captcha/refresh")) {
+                redirectToCaptcha(request, response, true);
             } else {
-                redirectToCaptcha(request, response);
+                response.setStatus(405);
             }
         } else {
             response.setStatus(405);
         }
     }
 
-    private void redirectToCaptcha(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    private void redirectToCaptcha(HttpServletRequest request, HttpServletResponse response, boolean refresh) throws IOException {
         HttpSession session = request.getSession();
-        int randomNumber = new Random().nextInt(899) + 100;
-        byte[] captcha = ImageUtils.toPng(Integer.toString(randomNumber));
+        if (refresh) {
+            session.removeAttribute(this.CAPTCHA_ATTRIBUTE_TEXT);
+            session.removeAttribute(this.CAPTCHA_ANSWER_ATTRIBUTE_TEXT);
+        }
 
-        session.setAttribute(this.CAPTCHA_ANSWER_ATTRIBUTE_TEXT, Integer.toString(randomNumber));
+        byte[] captcha;
+        if (session.getAttribute(this.CAPTCHA_ATTRIBUTE_TEXT) == null) {
+            int randomNumber = new Random().nextInt(899) + 100;
+            captcha = ImageUtils.toPng(Integer.toString(randomNumber));
+            session.setAttribute(this.CAPTCHA_ATTRIBUTE_TEXT, captcha);
+            session.setAttribute(this.CAPTCHA_ANSWER_ATTRIBUTE_TEXT, Integer.toString(randomNumber));
+        } else {
+            captcha = (byte[]) session.getAttribute(this.CAPTCHA_ATTRIBUTE_TEXT);
+        }
 
         response.setContentType("text/html");
         try (Writer writer = response.getWriter()) {
             String html = String.format("""
-                    <img width=237 height=97 src=data:image/png;base64,%s>
-                    <form action="captcha/check" method="post">
-                         <label for="captcha_input">Enter number:</label>
-                         <input name=%s id="captcha_input">
-                    </form>
-                    """, Base64.getEncoder().encodeToString(captcha), this.CAPTCHA_USER_ANSWER_PARAMETER_TEXT);
+                    <main style="height: 100%s; min-width: 400px; display: flex; flex-direction: column; align-items: center; justify-content: center;">
+                        <img width=180 height=90 src=data:image/png;base64,%s>
+                        <form action="/captcha/check" method="post">
+                             <label for="captcha_input">Enter number:</label>
+                             <input name=%s id="captcha_input autofocus">
+                        </form>
+                        <form action="/captcha/refresh" method="post">
+                            <button>Refresh</button>
+                        </form>
+                    </main>
+                    """, "%", Base64.getEncoder().encodeToString(captcha), this.CAPTCHA_USER_ANSWER_PARAMETER_TEXT);
             writer.write(html);
             writer.flush();
         }
